245adcb1e1ce3639a6fc8adbe23b319ef47de940
StoneMoe <lakechan96@gmail.com>
2016-07-05 18:32:11 +0800

    Show basic exception msg when error occured Add more output

--- getlink.py
+++ getlink.py
@@ -9,17 +9,25 @@ def log(content):
 
 
 def getlink(url, cookie=""):
+    log("Get hyperlinks from: %s" % url)
     try:
         response = urlopen(url)
 
         m = re.findall('<a.+?href="(.+?)"', response.read().decode("utf-8"), re.DOTALL)
+
         if len(m) < 1:
             log("No link found")
         else:
+            list = []
             for x in m:
-                log(x)
-    except Exception:
-        log("Error")
+                if x not in list:
+                    list.append(x)
+            for item in list:
+                log(item)
+
+    except Exception as e:
+        log("Error with %s: %s" % (url, e))
 
 if __name__ == "__main__":
+    getlink("a~.com")
     getlink("https://www.bilibili.com")
