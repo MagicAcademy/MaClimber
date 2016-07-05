e0df419066d11f5c8186bdb77b8401583bf98850
StoneMoe <lakechan96@gmail.com>
2016-07-05 18:14:14 +0800

    Moved to py3.5.2

--- getlink.py
+++ getlink.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-import urllib2
+from urllib.request import urlopen
 import re
 
 
 def log(content):
-    print "[log] %s" % content
+    print("[log] %s" % content)
 
 
 def getlink(url, cookie=""):
     try:
-        requestBody = urllib2.Request(url)
-        requestBody.add_header("Cookie", cookie)
-        requestBody.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36")
-        requestBody.add_header("Referer", "http://www.javlibrary.com/")
-        response = urllib2.urlopen(requestBody)
+        response = urlopen(url)
 
         m = re.findall('<a.+?href="(.+?)"', response.read().decode("utf-8"), re.DOTALL)
         if len(m) < 1:
