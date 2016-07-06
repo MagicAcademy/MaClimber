d75d53ec293e7104752708bc55a3a450c9785c9b
郭睿立 <cookedsteak708@gmail.com>
2016-07-06 12:21:37 +0800

    add new files

---
+++ core/connection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Conn(object):
+    """
+    数据库连接对象
+    更换db_config以更换数据库
+    连接session作为属性
+    """
+    db_config = 'mysql+pymysql://root:@localhost:3306/maclimber?charset=utf8'
+
+    session = None
+
+    def __init__(self):
+        if self.session is None:
+            config = self.db_config
+            engine = create_engine(config)
+            DBSession = sessionmaker(bind=engine)
+            self.session = DBSession()
+        else:
+            pass
+
+    def __del__(self):
+        if self.session is not None:
+            self.session.close()
+        else:
+            pass
+
+
--- core/spider.py
+++ core/spider.py
@@ -2,34 +2,42 @@
 import http.cookiejar
 import gzip
 import urllib.request
+import sys,re
 from bs4 import BeautifulSoup
 
 
 class Spider(object):
-    """
-    爬虫类,含有抽象的公用方法
-    """
 
-    def __init__(self, url, header):
+    """
+    爬虫母皇,含有抽象的公用方法
+    """
+    def __init__(self, url, header, max_depth=0):
         self.url = url
         self.header = header
+        self.maxDepth = max_depth
 
-    def getHtml(self, type='soup', coding='utf-8'):
+    def getHtml(self, rtn_type='soup', coding='utf-8'):
         cj = http.cookiejar.CookieJar()
         proc = urllib.request.HTTPCookieProcessor(cj)
         opener = urllib.request.build_opener(proc)
         result = []
-        for key, value in self.header.items():
-            elem = (key, value)
-            result.append(elem)
+        if self.header != "":
+            for key, value in self.header.items():
+                elem = (key, value)
+                result.append(elem)
         opener.addheaders = result
         op = opener.open(self.url)
+        http_message = op.info()
+        coding = http_message.get_content_charset()
+        if coding == None or coding == "":
+            coding = "utf-8"
         data = op.read()
         data = self.ungzip(data).decode(coding)
-        if type=='soup':
+        if rtn_type == 'soup':
             return BeautifulSoup(data, "html.parser")
         else:
-            return data.replace('\n', '')
+            return data
+
 
     def ungzip(self, data):
         try:
@@ -41,4 +49,23 @@ class Spider(object):
         return data
 
 
+    def getLink(self, maxcurrent_depth, whole_list=[]):
+        '''
+        @SDD 纯爬虫方法 需要抽象
+        :param maxcurrent_depth:
+        :param whole_list:
+        :return:
+        '''
+        try:
+            pass
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print("Error with %s at line %s: %s" % (self.url, exc_tb.tb_lineno, e))
 
+
+    def getTitle(self, htmlcontent):
+        m = re.findall('<title>(.+?)</title>', htmlcontent, re.DOTALL)
+        if len(m) < 1:
+            return "No title"
+        else:
+            return m[0]
