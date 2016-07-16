f7474e47d441354bb79f543bddd47eddd99f219a
neetdai <ake17101@gmail.com>
2016-07-16 15:54:58 +0800

    大致就完成了自己想表达的东西

--- core/spider.py
+++ core/spider.py
@@ -19,6 +19,8 @@ class Spider(object):
         self.__url = ""
         self.__config = []
         self.__currentDepth = 0
+        self.__domain = ""
+        self.__prot = ""
 
     def setFilters (self,filter):
         self.__filters.append(filter)
@@ -28,6 +30,18 @@ class Spider(object):
 
     def setConfig (self,config):
         self.__config = config
+
+    # 获取http地址
+    def filtersRun (self,html):
+        urls = []
+        for filter in self.__filters:
+            urls.extend(filter(html))
+        return urls
+
+    # 将获取的html内容提供给plugin使用
+    def pluginsRun (self,html):
+        for plugin in self.__plugins:
+            plugin(html)
 
     def getHtml (self,url):
         try:
@@ -40,6 +54,17 @@ class Spider(object):
                 return ""
 
             print("climbing %s" % (url))
+
+            if self.__prot == "":
+                if url.startswith("https:"):
+                    self.__prot = "https:"
+                else:
+                    self.__prot = "http:"
+
+            if self.__domain == "":
+                d = re.findall("(http(?:s|):\/\/.+?)(?:[\/]|$)", url, re.DOTALL)
+                if len(d) > 0:
+                    self.__domain = d[0]
 
             request = Request(self.__encodeUri(url))
 
@@ -66,12 +91,40 @@ class Spider(object):
             exc_type, exc_obj, exc_tb = sys.exc_info()
             print("Error with %s at line %s: %s" % (url, exc_tb.tb_lineno, e))
 
+    def getLink (self,urls):
+        try:
+            if len(urls) == 0:
+                return
+
+            urlList = urls[:self.__singlePageMaxTrace]
+            
+            self.__currentDepth += 1
+
+            for url in urlList:
+                if url.startswith("http"):
+                    self.Run(url)
+                elif url.startswith("//"):
+                    self.Run(self.__prot + url)
+                elif url.startswith("/"):
+                    self.Run(self.__domain + url)
+                elif url.startswith("#"):
+                    print("%s | %s" % (self.__domain + url,"[Dynamic link]"))
+                elif url.startswith("javascript"):
+                    print("ignore %s" % (url))
+                else:
+                    self.Run(self.__domain + "/" + url)
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print("Error with %s at line %s: %s" % (url, exc_tb.tb_lineno, e))
+
     def __encodeUri (self,url):
 
         result = parse.urlparse(url)
         
         # 将 query 部分按照 / 切割,然后将每个元素进行编码
-        pathTmp = list(map(lambda tmp:quote(tmp),result.path.split("/")))
+        # pathTmp = list(map(lambda tmp:quote(tmp),result.path.split("/")))
+        pathTmp = [quote(tmp) for tmp in result.path.split("/")]
 
         queryTmp = []
         for item in result.query.split("&"):
@@ -86,8 +139,17 @@ class Spider(object):
     def __ungzip (self,data):
         data = gzip.decompress(data)
         return data
+
+    def Run (self,url):
+        html = self.getHtml(url)
+        urls = self.filtersRun(html)
+        if len(self.__plugins) > 0:
+            self.__plugins(html)
+        return self.getLink(urls)
+
 if __name__ == '__main__':
     s = Spider()
-    # s.encodeUri("http://www.baidu.com/")
     s.setConfig([("Accept-Encoding","gzip"),("Connection","keep-alive"),("Cache-Control","max-age=0")])
-    s.getHtml("http://www.baidu.com/")
+    s.setFilters(lambda html:re.findall('<a.+?href="(.+?)"', html, re.DOTALL))
+    s.Run("http://www.acfun.tv/")
+    
